f6d0c91df240f686b81fcac1b1e422bd5a70b708
Balint Szebenyi <szebenyib@gmail.com>
2015-10-27 21:33:57 +0100

    Added text

--- 09_data_products/cp/ui.R
+++ 09_data_products/cp/ui.R
@@ -3,6 +3,7 @@ shinyUI(pageWithSidebar(
   headerPanel("Quick Regression via Shiny"),
   sidebarPanel(
     h3('Settings'),
+    p('Use the following controls to setup the multivariate and model parameters.'),
     selectInput('dependent_var', 'Dependent variable',
                 choices = "salary",
 #restriction                choices = names(df),
@@ -20,7 +21,18 @@ shinyUI(pageWithSidebar(
     tabsetPanel(position = "above",
       tabPanel("Description",
                p('This page was created as my course project
-                           for the coursera class: Developing data products.')),
+                           for the coursera class: Developing data products.'),
+               p('Navigate along the sections to gain insight into the data.'),
+               p('The univariate section provides a univariate analysis.
+                 By choosing the desired variable a plot is constructed. In case
+                 of categorical variables this is violin plot, in case of
+                 continuous variables this is a simple histogram. Alongside
+                 the chart simple statistics are also computed.'),
+               p('The multivariate section creates a scatterplot for all
+                 variables and also provides some additional information'),
+               p('The model section reveals a simple regression model with the
+                possibility given to the user to choose the explanatory
+                 variables.')),
       tabPanel("Univariate",
                selectInput(inputId = "univariate_var",
                            label = "Choose a variable to analyze",
@@ -29,7 +41,7 @@ shinyUI(pageWithSidebar(
                plotOutput('uni_plot')),
                #summary
                #plot
-      tabPanel("Scatterplot",
+      tabPanel("Multivariate",
                plotOutput('fit_plot')),
       tabPanel("Model",
                h3('Model Summary'),
